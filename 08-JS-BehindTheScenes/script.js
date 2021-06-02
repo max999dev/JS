26db00eb8f653b69badc69140f0d504b1cdcf572
Max <sveramaxim@gmail.com>
2021-06-02 10:55:08 -0500

    JS behind the scenes

---
+++ 08-JS-BehindTheScenes/script.js
@@ -0,0 +1,37 @@
+'use strict';
+
+// function calcAge(birthYear){
+//     const age = 2039 - birthYear;
+
+//     function printAge(){
+//         const output = `You are ${age}, born in ${birthYear}`;
+//         console.log(output);
+//     }
+//      printAge();
+//     return age;
+// }
+
+// console.log(calcAge(1988));
+
+
+// const Max = {
+//     firstName:'Max', 
+//     lastName:'Smith', 
+//     calcAge: function () {
+// console.log(2031-1988);
+//     }
+// }
+
+// Max.calcAge();
+
+// const Mariia = {
+//   firstName: "Mariia",
+//   lastName: "Smith",
+// };
+
+// Max.calcAge = Mariia.calcAge; // this is called method borrowing 
+
+
+// Difference between function and arrow function
+
+
